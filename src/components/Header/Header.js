b39bad0b7bbc05173d8c646bb7a28fbccd6317d8
alinu1 <uwu1343@gmail.com>
2023-04-26 23:01:45 +0600

    components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+        <header>
+        <h2>logo</h2>
+        <nav>
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+        </nav>
+      </header>
+  )
+}
